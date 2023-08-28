7b7d9affc1337aba7061769a2abac5b5e637474b
TOUZOUZ Adnane <115072337+T-adnane@users.noreply.github.com>
2023-08-28 12:13:56 +0100

    Add files via upload

--- personal-portfolio - Copie/src/components/Banner.js
+++ personal-portfolio - Copie/src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/headerimg.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Votre Solution", "de Segmentation", "Dentaire." ];
+  const toRotate = [ "Votre Solution de Segmentation Dentaire." ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,9 +56,9 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Bienvenue dans SmileSegment</span>
-                <h1>{`Votre Solution`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Votre Solution", "de Segmentation", "Dentaire"]'><span className="wrap">{text}</span></span></h1>
+                <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Votre Solution", "de Segmentation", "Dentaire"]'><span className="wrap">{text}</span></span></h1>
                   <p>Nous sommes ravis de vous accueillir dans l'univers de SmileSegment, où la technologie rencontre le sourire.</p>
-                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  <button onClick={() => console.log('connect')}>Let’s Start <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
           </Col>
--- personal-portfolio - Copie/src/components/NavBar.js
+++ personal-portfolio - Copie/src/components/NavBar.js
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import logo from '../assets/img/logo.png';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -49,13 +46,8 @@ export const NavBar = () => {
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Let’s Start</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
